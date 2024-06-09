860bda9886f89365a66a5b51d03faa746f582dcd
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-06-09 13:13:07 +0200

    spring project initiated

---
+++ src/main/java/com/example/books_tracker/BooksTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.books_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/books_tracker/BooksTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.books_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
