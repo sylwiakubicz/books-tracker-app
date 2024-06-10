35f163cc6251a77ef4dfa1da657f16ad870aeb49
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-06-10 22:19:26 +0200

    Utworzono repozytorium dla encji Authors. Dodano metodę wyszukiwania rekordu po kolumnie Name i Surname.

---
+++ src/main/java/com/example/books_tracker/repository/AuthorsRepository.java
@@ -0,0 +1,13 @@
+package com.example.books_tracker.repository;
+
+import com.example.books_tracker.model.Authors;
+import com.example.books_tracker.model.Genres;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorsRepository extends JpaRepository<Authors, Long> {
+    Optional<Authors> findByNameAndSurname(String name, String surname);
+}
