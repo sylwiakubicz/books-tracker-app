39eb4a75c538cdd25cd534ac0b24ab70c765fb75
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-06-30 14:29:16 +0200

    Dodano nową metodę w booksrepository, służącą do sprawdzania po kolumnie ISBN, czy dana książka istnieje w bazie danych. Zaaktualizowano post metodę dodawania nowej książki do bazy danych o weryfikację, czy książki o danym kodzie ISBN nie ma już przypadkiem w bazie danych.

--- src/main/java/com/example/books_tracker/controller/BooksController.java
+++ src/main/java/com/example/books_tracker/controller/BooksController.java
@@ -1,24 +1,26 @@
 package com.example.books_tracker.controller;
 
 import com.example.books_tracker.model.Books;
+import com.example.books_tracker.repository.BooksRepository;
 import com.example.books_tracker.service.BooksService;
-import jakarta.persistence.GeneratedValue;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BooksController {
 
     private final BooksService booksService;
+    private final BooksRepository booksRepository;
 
-    public BooksController(BooksService booksService) {
+    public BooksController(BooksService booksService, BooksRepository booksRepository) {
         this.booksService = booksService;
+        this.booksRepository = booksRepository;
     }
 
     @GetMapping
@@ -38,8 +40,12 @@ public class BooksController {
     }
 
     @PostMapping
-    public Books create(@RequestBody Books books) {
-        return booksService.create(books);
+    public ResponseEntity<String> create(@RequestBody Books books) {
+        if (booksRepository.existsBooksByISBN(books.getISBN())) {
+            return new ResponseEntity<>("Book existed!", HttpStatus.BAD_REQUEST);
+        }
+        booksService.create(books);
+        return new ResponseEntity<>("Book created successfully", HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
