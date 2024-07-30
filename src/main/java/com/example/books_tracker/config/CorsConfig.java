562107a452b7a0d9dcdc9f89226d017ac5b76edc
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-07-30 23:11:16 +0200

    Zazwolono na wykonywanie requestów z "http://localhost:4200".

--- src/main/java/com/example/books_tracker/config/CorsConfig.java
+++ src/main/java/com/example/books_tracker/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost")
+                        .allowedOrigins("http://localhost", "http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Authorization", "Content-Type")
                         .allowCredentials(true);
