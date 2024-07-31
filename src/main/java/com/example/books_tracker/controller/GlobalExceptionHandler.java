ed67766b3bb6aa0e5f42464f72538e3c5f66a579
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-07-31 20:55:37 +0200

    Utworzono klasę do globalnej obsługi wyjątków aplikacji. Obsługiwane wyjątki: UserAlreadyExistsException, MethodArgumentNotValidException oraz ConstraintViolationException. Wyjątki zwracają response entity z wiadomością o wyjątku w postaci JSON.

---
+++ src/main/java/com/example/books_tracker/controller/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.example.books_tracker.controller;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<?> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return new ResponseEntity<>(Map.of("message", ex.getMessage()), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        return new ResponseEntity<>(Map.of("message", errorMessage), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException ex) {
+        String errorMessage = ex.getConstraintViolations().stream()
+                .map(violation -> violation.getMessage())
+                .collect(Collectors.joining(", "));
+        return new ResponseEntity<>(Map.of("message", errorMessage), HttpStatus.BAD_REQUEST);
+    }
+
+}
