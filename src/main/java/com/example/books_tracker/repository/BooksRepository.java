3d9ae648c90415b969f920590bfa68b04e4535ea
sylwiakubicz <sylwiakubicz31@gmail.com>
2024-08-24 09:04:52 +0200

    Dodano 3 nowe metody do BooksRepository: do wyszukiwania minimalnej i maksymalnej wartości id w tabeli, wyszukującą rekordy na podstawie listy id.

--- src/main/java/com/example/books_tracker/repository/BooksRepository.java
+++ src/main/java/com/example/books_tracker/repository/BooksRepository.java
@@ -3,10 +3,23 @@ package com.example.books_tracker.repository;
 import com.example.books_tracker.model.Books;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BooksRepository extends JpaRepository<Books, Long>, JpaSpecificationExecutor<Books> {
     Boolean existsBooksByISBN(String isbn);
     Boolean existsBooksByBookId(Long id);
+
+    @Query("SELECT MIN(e.bookId) FROM Books e")
+    Long findMinId();
+
+    @Query("SELECT MAX(e.bookId) FROM Books e")
+    Long findMaxId();
+
+    @Query("SELECT e FROM Books e WHERE e.bookId IN :ids")
+    List<Books> findByIds(@Param("ids") List<Long> ids);
 }
